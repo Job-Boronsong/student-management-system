ffbe841c03c3721e3e8a956cddbc20c0bdbb3142
Job Boronsong <jboronsong@gmail.com>
2025-09-22 12:55:38 +0000

    Add

--- school/views.py
+++ school/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseForbidden
 from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Notification
